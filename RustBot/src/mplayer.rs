1c62e7004b9394230fe02b5a434431eb4d642030
CamiFio <cfiorotto@fi.uba.ar>
2022-06-04 09:23:48 -0300

    ..

--- RustBot/src/mplayer.rs
+++ RustBot/src/mplayer.rs
@@ -124,7 +124,7 @@ async fn add_queue() {}
 // Devuelve el string que contiene la cancion a ser buscada
 async fn get_song(ctx: &Context, msg: &Message) -> String {
     let input = String::from(&msg.content);
-    let song = input.replace("!play ", "");
+    let song = input.replace("!play", " ");
     String::from(song)
 }
 
